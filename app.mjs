b0e7abaa8ea87c7d930fe295c4d4d2a327fa68ca
carlos-iso <carlosbalak2002@gmail.com>
2024-03-03 21:12:07 -0300

    Init

---
+++ app.mjs
@@ -0,0 +1,48 @@
+import createHttpError from "http-errors";
+import express from "express";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+
+import indexRouter from "./routes/index.mjs";
+import usersRouter from "./routes/users.mjs";
+
+var app = express();
+
+// view engine setup
+app.set("views", join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createHttpError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+export {
+  app
+};
